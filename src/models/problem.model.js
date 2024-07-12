9a63f4679f149277b3094a54fac0ffa592ad4162
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2024-07-12 20:45:40 +0530

    Feat :Completed create problem api

--- src/models/problem.model.js
+++ src/models/problem.model.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose');
 
 const problemSchema = new mongoose.Schema({
-    title : {
-        type : String,
-        required : [true , 'Title cannot be empty']
-    },
-    description :{
-        type : String,
-        required : [true , 'Description cannot be empty']
-    },
-    difficulty :{
+    title: {
         type: String,
-        enum:['Easy','Medium','Hard'],
-        required : [true , 'Difficulty cannot be empty'],
-        default : 'easy',
+        required: [true, 'Title cannot be empty']
     },
-    testCases : [{
-        input : {
-            type : String,
-            required : true
-        },
-        output:{
-            type : String,
-            required : true
+    description: {
+        type: String,
+        required: [true, 'Description cannot be empty']
+    },
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        required: [true, 'Difficulty cannot be empty'],
+        default: 'easy'
+    },
+    testCases: [
+        {
+            input: {
+                type: String,
+                required: true
+            },
+            output: {
+                type: String,
+                required: true
+            }
         }
-
-    }],
-    editorial:{
-            type : String
+    ],
+    editorial: {
+        type: String
     }
 });
 
-const Problem = mongoose.model('Problem',problemSchema);
+const Problem = mongoose.model('Problem', problemSchema);
 
-module.exports = Problem; 
+module.exports = Problem;
--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,19 +1,47 @@
-const Problem = require('../models');
+const NotFound = require('../errors/notfound.error');
+const { Problem } = require('../models');
 
-class ProblemRepository{
-    async createProblem(problemData){
+class ProblemRepository {
+
+    async createProblem(problemData) {
         try {
+
             const problem = await Problem.create({
-                title : problemData.title,
-                description :problemData.description,
-                testCases : (problemData.testCases) ? problemData.testCases : []
+                title: problemData.title,
+                description: problemData.description,
+                testCases: (problemData.testCases) ? problemData.testCases : []
             });
+
+            return problem;
+        } catch(error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getAllProblems() {
+        try {
+            const problems = await Problem.find({});
+            return problems;
+        } catch(error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    async getProblem(id) {
+        try {
+            const problem = await Problem.findById(id);
+            if(!problem) {
+                throw new NotFound("Problem", id);
+            }
             return problem;
         } catch (error) {
             console.log(error);
             throw error;
         }
     }
+
 }
 
 module.exports = ProblemRepository;
